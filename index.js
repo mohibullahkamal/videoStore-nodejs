c9d73d76c911158aeb3f3fab7a650f16572d65ed
mogit2020 <mohibullah.kamal@gmail.com>
2019-01-19 17:38:01 +0100

    add .get

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
 
 module.exports = server;
