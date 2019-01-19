c373798567e1ac891395cc73ad0d17882c07fa1e
mogit2020 <mohibullah.kamal@gmail.com>
2019-01-19 19:02:30 +0100

    add courses array

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+
+const courses = [
+    { id: 1, name: 'animation' },
+    { id: 2, name: 'crime' },
+    { id: 3, name: 'documentary' },
+    { id: 4, name: 'drama' },
+    { id: 5, name: 'horror' },
+];
+
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
@@ -10,4 +20,21 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
 
-module.exports = server;
+
+
+
+
+
+
+
+
+
+
+
+
+// PORT - it is unlikely that our hosting port will be 3000... therefore to set it dynamically we use this process.env method... 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {   // we set a listener... 
+    console.log(`Listening on PORT ${port}...`);
+});
+// module.exports = server;
